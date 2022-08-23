faee961defaaf49c9b5abd6deafbb5ffc3cb6f32
Adriano Reis <adrianoreispereira88@gmail.com>
2022-08-23 01:29:21 -0300

    Mudanção de utilitarios

--- core/utils.h
+++ core/utils.h
@@ -9,8 +9,8 @@
 
 #define hideCursor pritnf("\x1b[?25l")              // Hide cursor of terminal.
 #define showCursor pritnf("\x1b[?25h")              // Show cursor of terminal.
-#define clearScreen printf("\x1b[2J\x1b[H")         // Clear screen to render the game.
-#define gotoXY(x, y) printf("\x1b[%d;%dH", x, y)    // Move the cursor in screen.
+#define clearScreen printf("\x1b[3J\x1b[H")         // Clear screen to render the game.
+#define gotoXY(x, y) printf("\x1b[%d;%df", x, y)    // Move the cursor in screen.
 
 // Set Game Loop.
 
